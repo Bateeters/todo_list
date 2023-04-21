70cd2db29bbf7a6fd8ba12aa89acd7f5db8a9e50
Brian <brianteetersdesign@gmail.com>
2023-04-21 10:42:24 -0400

    Removed PW restrictions for demonstration purposes

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -85,14 +85,15 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
+
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
-    },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", },
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator", },
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator", },
+    #    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",},
+    #    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", },
+    #    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator", },
+    #    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator", },
 ]
+
+# Commented out above to disable password restrictions for testing and demonstration purposes
 
 
 # Internationalization
